52358591f13437d4983d9d7fad0bfd4dd603cc66
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-09-08 12:23:32 +0500

    Initial commit

---
+++ src/app/interaction/interaction.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interaction',
+  imports: [],
+  templateUrl: './interaction.html',
+  styleUrl: './interaction.css'
+})
+export class Interaction {
+
+}
